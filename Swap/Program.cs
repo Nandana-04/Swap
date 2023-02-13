a760d9d0e76973c23ec3811a8a5ff71021014c0e
Nandana S <nandana.s@experionglobal.local>
2023-02-13 15:28:51 +0530

    msg

---
+++ Swap/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3 ; 
+            int b = 4;
+            int temp;
+
+            temp = a ;
+            a = b ;
+            b = temp ;
+
+            Console.WriteLine("After swaping :");
+            Console.WriteLine("First number : " + a);
+            Console.WriteLine("Second number :" + b);
+
+        }
+    }
+}
